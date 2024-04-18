371d9357d259bee9b49a188bca88533fe11a82b4
“axzilla” <hello@axeladrian.com>
2024-04-18 17:18:01 +0200

    refactor: create component for dashboard-card

---
+++ web/src/lib/components/containers/dashbard-card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+
+	export let title: string;
+	export let value: string;
+	export let subValue: string;
+</script>
+
+<Card.Root>
+	<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+		<Card.Title class="text-sm font-medium">{title}</Card.Title>
+		<slot name="icon" />
+	</Card.Header>
+	<Card.Content>
+		<div class="text-2xl font-bold">{value}</div>
+		<p class="text-xs text-muted-foreground">{subValue}</p>
+	</Card.Content>
+</Card.Root>
--- web/src/routes/(app)/+page.svelte
+++ web/src/routes/(app)/+page.svelte
@@ -9,49 +9,22 @@
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
 	import * as Table from '$lib/components/ui/table';
+	import DashbardCard from '$lib/components/containers/dashbard-card.svelte';
 </script>
 
 <div class="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-	<Card.Root>
-		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
-			<DollarSign class="h-4 w-4 text-muted-foreground" />
-		</Card.Header>
-		<Card.Content>
-			<div class="text-2xl font-bold">$45,231.89</div>
-			<p class="text-xs text-muted-foreground">+20.1% from last month</p>
-		</Card.Content>
-	</Card.Root>
-	<Card.Root>
-		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-			<Card.Title class="text-sm font-medium">Subscriptions</Card.Title>
-			<Users class="h-4 w-4 text-muted-foreground" />
-		</Card.Header>
-		<Card.Content>
-			<div class="text-2xl font-bold">+2350</div>
-			<p class="text-xs text-muted-foreground">+180.1% from last month</p>
-		</Card.Content>
-	</Card.Root>
-	<Card.Root>
-		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-			<Card.Title class="text-sm font-medium">Sales</Card.Title>
-			<CreditCard class="h-4 w-4 text-muted-foreground" />
-		</Card.Header>
-		<Card.Content>
-			<div class="text-2xl font-bold">+12,234</div>
-			<p class="text-xs text-muted-foreground">+19% from last month</p>
-		</Card.Content>
-	</Card.Root>
-	<Card.Root>
-		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-			<Card.Title class="text-sm font-medium">Active Now</Card.Title>
-			<Activity class="h-4 w-4 text-muted-foreground" />
-		</Card.Header>
-		<Card.Content>
-			<div class="text-2xl font-bold">+573</div>
-			<p class="text-xs text-muted-foreground">+201 since last hour</p>
-		</Card.Content>
-	</Card.Root>
+	<DashbardCard title="Total Revenue" value="$45,231.90" subValue="+20% from last moth">
+		<DollarSign slot="icon" class="h-4 w-4 text-muted-foreground" />
+	</DashbardCard>
+	<DashbardCard title="Subscriptions" value="+2350" subValue="+189.1% from last month">
+		<Users slot="icon" class="h-4 w-4 text-muted-foreground" />
+	</DashbardCard>
+	<DashbardCard title="Sales" value="+2350" subValue="+19% from last month">
+		<CreditCard slot="icon" class="h-4 w-4 text-muted-foreground" />
+	</DashbardCard>
+	<DashbardCard title="Active Now" value="+572" subValue="+201 since last hour">
+		<Activity slot="icon" class="h-4 w-4 text-muted-foreground" />
+	</DashbardCard>
 </div>
 <div class="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
 	<Card.Root class="xl:col-span-2">
