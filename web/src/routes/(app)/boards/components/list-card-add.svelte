a44f3c0dc13350487ce5e39e2206cfe7d555e598
“axzilla” <hello@axeladrian.com>
2024-05-03 16:45:56 +0200

    even more on UI

--- web/src/routes/(app)/boards/components/list-card-add.svelte
+++ web/src/routes/(app)/boards/components/list-card-add.svelte
@@ -3,22 +3,29 @@
 	import { ListForm } from '.';
 	import { clickOutside } from '$lib/utils';
 	import type { PageData } from '../$types';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { CirclePlus } from 'lucide-svelte';
 
 	export let data: PageData;
 
 	let isFormOpen = false;
 </script>
 
-<Card class="bg-muted min-w-72 h-16 items-center flex justify-between gap-2 p-2">
-	{#if isFormOpen}
-		<div class="w-full" use:clickOutside={() => (isFormOpen = false)}>
-			<ListForm bind:isFormOpen {data} list={null} />
-		</div>
-	{:else}
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div class="w-full cursor-pointer" on:click={() => (isFormOpen = true)}>
-			<CardTitle class="text-sm font-bold p-2">Add new list</CardTitle>
-		</div>
-	{/if}
-</Card>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:click={() => (isFormOpen = true)}>
+	<Card class="cursor-pointer bg-muted min-w-72 h-16 items-center flex justify-between gap-2 p-2">
+		{#if isFormOpen}
+			<div class="w-full" use:clickOutside={() => (isFormOpen = false)}>
+				<ListForm bind:isFormOpen {data} list={null} />
+			</div>
+		{:else}
+			<div class="flex items-center" on:click={() => (isFormOpen = true)}>
+				<Button size="icon" variant="ghost" disabled type="submit">
+					<CirclePlus class="h-5 w-5" />
+				</Button>
+				<CardTitle class="text-sm font-bold p-2">Add new list</CardTitle>
+			</div>
+		{/if}
+	</Card>
+</div>
