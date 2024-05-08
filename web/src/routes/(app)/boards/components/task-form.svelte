966d2ee0f7ef05da086a7cc7b5029e6dc0663c8d
“axzilla” <hello@axeladrian.com>
2024-05-08 08:07:33 +0200

    feat: tasks, implement prio

--- web/src/routes/(app)/boards/components/task-form.svelte
+++ web/src/routes/(app)/boards/components/task-form.svelte
@@ -3,16 +3,31 @@
 	import { Dialog, DialogContent, DialogHeader, DialogTitle } from '$lib/components/ui/dialog';
 	import { FormControl, FormField, FormFieldErrors, FormLabel } from '$lib/components/ui/form';
 	import { Input } from '$lib/components/ui/input';
+	import {
+		Select,
+		SelectContent,
+		SelectItem,
+		SelectTrigger,
+		SelectValue
+	} from '$lib/components/ui/select';
 	import { Textarea } from '$lib/components/ui/textarea';
 	import { TaskSchema, type List, type Task } from '$lib/schemas';
 	import { tasks } from '$lib/stores';
 	import { Trash } from 'lucide-svelte';
-	import SuperDebug, { defaultValues, superForm } from 'sveltekit-superforms';
+	import { defaultValues, superForm } from 'sveltekit-superforms';
 	import { zod } from 'sveltekit-superforms/adapters';
 
 	export let task: Task | null;
 	export let list: List | null;
 	export let isTaskFormOpen = false;
+
+	const priorities = [
+		{ value: 'low', label: 'Low' },
+		{ value: 'medium', label: 'Medium' },
+		{ value: 'high', label: 'High' }
+	];
+	let selectedPriority =
+		priorities.find((p) => p.value === (task ? task.priority : 'low')) || priorities[0];
 
 	const form = superForm(
 		task || { ...defaultValues(zod(TaskSchema)), list_id: list ? list.id : '' },
@@ -48,25 +63,47 @@
 
 <Dialog bind:open={isTaskFormOpen}>
 	<DialogContent class="sm:max-w-[425px]">
-		<SuperDebug data={formData} />
 		<DialogHeader>
 			<DialogTitle>{!task ? 'Create' : 'Edit'} Task</DialogTitle>
 		</DialogHeader>
 		<form action="?/createOrUpdateTask" method="POST" use:enhance>
-			<FormField {form} name="title">
-				<FormControl let:attrs>
-					<FormLabel>Title</FormLabel>
-					<Input autofocus {...attrs} bind:value={$formData.title} />
-				</FormControl>
-				<FormFieldErrors />
-			</FormField>
-			<FormField {form} name="description">
-				<FormControl let:attrs>
-					<FormLabel>Description</FormLabel>
-					<Textarea {...attrs} bind:value={$formData.description} />
-				</FormControl>
-				<FormFieldErrors />
-			</FormField>
+			<div class="pb-4">
+				<FormField {form} name="title">
+					<FormControl let:attrs>
+						<FormLabel>Title</FormLabel>
+						<Input autofocus {...attrs} bind:value={$formData.title} />
+					</FormControl>
+					<FormFieldErrors />
+				</FormField>
+				<FormField {form} name="description">
+					<FormControl let:attrs>
+						<FormLabel>Description</FormLabel>
+						<Textarea {...attrs} rows={4} bind:value={$formData.description} />
+					</FormControl>
+					<FormFieldErrors />
+				</FormField>
+				<FormField {form} name="priority">
+					<FormControl let:attrs>
+						<FormLabel>Priority</FormLabel>
+						<Select
+							selected={selectedPriority}
+							onSelectedChange={(v) => {
+								v && ($formData.priority = v.value);
+							}}
+						>
+							<SelectTrigger {...attrs}>
+								<SelectValue placeholder="Select a priority for this task" />
+							</SelectTrigger>
+							<SelectContent>
+								{#each priorities as { value, label }}
+									<SelectItem {value} {label} />
+								{/each}
+							</SelectContent>
+						</Select>
+					</FormControl>
+					<FormFieldErrors />
+				</FormField>
+			</div>
 			<div class="flex justify-between">
 				{#if $formData.id}
 					<Button
@@ -78,6 +115,8 @@
 					>
 						<Trash class="h-5 w-5" />
 					</Button>
+				{:else}
+					<div />
 				{/if}
 				<div>
 					<Button
