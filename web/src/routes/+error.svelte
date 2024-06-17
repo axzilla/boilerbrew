0b673e0ba54720177e91919578f40a797802b86b
“axzilla” <hello@axeladrian.com>
2024-06-17 15:03:38 +0200

    feat: create global 404 page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-screen flex items-center justify-center">
+	<div>
+		<pre class="flex justify-center">
+^__^
+(oo)\_______
+(__)\       )\/\
+     ||----w |
+     ||     ||
+     </pre>
+		<h1 class="text-4xl text-center font-bold mt-4">{$page.status}</h1>
+		<h2 class="text-2xl text-center text-gray-600 mt-2">{$page.error?.message}</h2>
+	</div>
+</div>
