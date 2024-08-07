9855122972b29cb43a83c08914b51f9345d517de
“axzilla” <hello@axeladrian.com>
2024-08-07 14:20:19 +0200

    fix: login error handling

--- web/src/routes/(auth)/login/+page.server.ts
+++ web/src/routes/(auth)/login/+page.server.ts
@@ -1,8 +1,9 @@
 import { fail, type Actions } from '@sveltejs/kit';
 import { LoginUserSchema } from '$lib/schemas';
 import type { PageServerLoad } from './$types.js';
-import { superValidate, setError, message } from 'sveltekit-superforms';
+import { superValidate, setError } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import { ClientResponseError } from 'pocketbase';
 
 export const load: PageServerLoad = async () => {
 	return {
@@ -13,25 +14,43 @@ export const load: PageServerLoad = async () => {
 export const actions: Actions = {
 	login: async ({ request, locals }) => {
 		const form = await superValidate(request, zod(LoginUserSchema));
-
 		if (!form.valid) {
 			return fail(400, { form });
 		}
-
 		const formData = form.data;
-
 		try {
 			await locals.pb.collection('users').authWithPassword(formData.login, formData.password);
 
 			if (!locals.pb?.authStore?.model?.verified) {
 				locals.pb.authStore.clear();
-				throw message(form, 'Please verify your email address.');
+				return setError(form, 'login', 'Please verify your email address.');
 			}
 
 			return { form };
-		} catch (err) {
-			console.log('Error: ', err);
-			return setError(form, '');
+		} catch (err: unknown) {
+			console.error('Login error:', err);
+
+			if (err instanceof ClientResponseError) {
+				// PocketBase specific error
+				if (err.status === 400) {
+					// Set error for both login and password fields
+					setError(form, 'login', 'Invalid email or password');
+					setError(form, 'password', 'Invalid email or password');
+					return fail(400, { form });
+				} else {
+					return setError(form, '', err.message);
+				}
+			} else if (err instanceof Error) {
+				// Generic Error object
+				return setError(
+					form,
+					'',
+					err.message || 'An unexpected error occurred. Please try again later.'
+				);
+			} else {
+				// Unknown error type
+				return setError(form, '', 'An unexpected error occurred. Please try again later.');
+			}
 		}
 	}
 };
