6c1522c57d2082c554c683b0fae9be4880227b85
“axzilla” <hello@axeladrian.com>
2024-08-12 10:44:08 +0200

    fix: goal card dnd switch bug

--- web/src/routes/api/goal/update-index/+server.ts
+++ web/src/routes/api/goal/update-index/+server.ts
@@ -2,18 +2,41 @@ import type { Goal } from '$lib/schemas';
 import { json } from '@sveltejs/kit';
 
 export const POST = async ({ locals, request }: { locals: App.Locals; request: Request }) => {
-	const userId = locals.user?.id;
-	const { goals }: { goals: Goal[] } = await request.json();
+	try {
+		const userId = locals.user?.id;
+		const { goals }: { goals: Goal[] } = await request.json();
 
-	const updatePromises = goals.map((item, index) =>
-		locals.pb.collection('goals').update(item.id, { index })
-	);
+		const updatePromises = goals.map((item, index) =>
+			locals.pb.collection('goals').update(item.id, { index })
+		);
 
-	await Promise.all(updatePromises);
+		await Promise.all(updatePromises);
 
-	const updatedGoals: Goal[] = await locals.pb
-		.collection('goals')
-		.getFullList({ filter: `user_id = "${userId}"` });
+		const goalsWithExpandedMilestones = await locals.pb.collection('goals').getFullList({
+			filter: `user_id = "${userId}"`,
+			expand: 'milestones_via_goal_id',
+			sort: '-created'
+		});
 
-	return json(updatedGoals);
+		const updatedGoals: Goal[] = goalsWithExpandedMilestones
+			.map((goal) => {
+				const { expand, ...restGoal } = goal;
+				return {
+					...restGoal,
+					title: restGoal.title || '',
+					description: restGoal.description || '',
+					progress: restGoal.progress || 0,
+					index: restGoal.index || 0,
+					created: restGoal.created || '',
+					updated: restGoal.updated || '',
+					milestones: expand?.milestones_via_goal_id || []
+				};
+			})
+			.sort((a, b) => (a.created > b.created ? -1 : 1));
+
+		return json(updatedGoals);
+	} catch (err) {
+		console.error('Unexpected error:', err);
+		return json({ error: 'Unexpected error' }, { status: 500 });
+	}
 };
