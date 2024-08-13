59c298aa87ad2a52ab55b5dc44991538239e9c9c
“axzilla” <hello@axeladrian.com>
2024-08-13 11:21:43 +0200

    feat: landing, refine navbar

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -3,34 +3,34 @@
 		Menu,
 		Activity,
 		ArrowUpRight,
-		CircleUser,
 		CreditCard,
 		DollarSign,
 		Package2,
-		Search,
 		Users
 	} from 'lucide-svelte/icons';
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
 	import * as Sheet from '$lib/components/ui/sheet/index.js';
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { ThemeSwitcher } from '$lib/components/modules/index';
 </script>
 
 <div class="flex min-h-screen w-full flex-col">
-	<header class="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6">
-		<nav
-			class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
-		>
+	<header
+		class="bg-background sticky top-0 flex h-16 justify-between items-center gap-4 border-b px-4 md:px-6 w-full"
+	>
+		<div class="flex gap-2">
 			<a href="##" class="flex items-center gap-2 text-lg font-semibold md:text-base">
 				<Package2 class="h-6 w-6" />
 				<span class="sr-only">Acme Inc</span>
 			</a>
 			<a href="##" class="text-foreground hover:text-foreground transition-colors"> Dashboard </a>
+		</div>
+		<nav
+			class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
+		>
 			<a href="##" class="text-muted-foreground hover:text-foreground transition-colors">
 				Orders
 			</a>
@@ -65,36 +65,8 @@
 				</nav>
 			</Sheet.Content>
 		</Sheet.Root>
-
-		<div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-			<form class="ml-auto flex-1 sm:flex-initial">
-				<div class="relative">
-					<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-					<Input
-						type="search"
-						placeholder="Search products..."
-						class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-					/>
-				</div>
-			</form>
+		<div class="flex items-center gap-4 md:gap-2 lg:gap-4">
 			<ThemeSwitcher />
-
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger asChild let:builder>
-					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
-						<CircleUser class="h-5 w-5" />
-						<span class="sr-only">Toggle user menu</span>
-					</Button>
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content align="end">
-					<DropdownMenu.Label>My Account</DropdownMenu.Label>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Settings</DropdownMenu.Item>
-					<DropdownMenu.Item>Support</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Logout</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
 		</div>
 	</header>
 	<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
