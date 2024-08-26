fe81ccda90082dd8094e48ed37dd43676efddcd6
“axzilla” <hello@axeladrian.com>
2024-08-26 15:42:20 +0200

    hotfix: layout issue

--- web/src/routes/(landing)/+layout.svelte
+++ web/src/routes/(landing)/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import '../../app.postcss';
-	import { Toaster } from '$lib/components/ui/sonner';
 	import { LandingLayout } from '$lib/components/layouts';
 </script>
 
-<Toaster richColors />
 <LandingLayout>
 	<slot />
 </LandingLayout>
---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { Toaster } from '$lib/components/ui/sonner';
+</script>
+
+<Toaster richColors />
+<slot />
